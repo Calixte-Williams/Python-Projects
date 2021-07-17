af485e10e15c388bfffa0884a6a5c771e3e97768
Calixte Williams <calixte.williams@gmail.com>
2021-07-17 13:15:07 -0400

    Fixed Guessing Game and Complicated the wording a bit

--- Guess The Number/Guessing Game.py
+++ Guess The Number/Guessing Game.py
@@ -1,11 +1,5 @@
 import random
 
-lvl1 = 10
-lvl2 = 30
-lvl3 = 60
-lvl4 = 100
-signal = 0
-
 def guess(x):
     answer = random.randint(1, signal)
     guess = 0
@@ -16,15 +10,18 @@ def guess(x):
         elif guess > answer:
             print('Guess again, you were too high.')
     print(f'Congrats, you guessed {answer} correclty!!! You Won!!')
+    exit()
 
-print("Welcome to the Guessing Game"
-"-----------------------------"
-"Guess the correct number and you win... Easy Right!?")
-print("Difficulty Levels:" 
+print("Welcome to the Guessing Game")
+print("-----------------------------")
+print("Guess the correct number and you win... Easy")
+print("")
+print("Difficulty Levels: " 
 "Level 1 = 1 - 10 "
 "Level 2 = 1 - 30 "
 "Level 3 = 1 - 60 "
 "Level 4 = 1 - 100")
+
 
 difflvl = int(input("Choose your difficulty from 1 to 4: "))  #lets player select the difficulty
 if difflvl == 1:
@@ -36,12 +33,6 @@ if difflvl == 2:
 if difflvl == 3:
         signal = 60
         guess(signal)
-else difflvl == 4:
-        signal = 100
-        guess(signal)
-
-
-#int(signal)
-#print(difflvl)
-#print(signal)
-#guess(signal)
+else: 
+    signal = 100
+    guess(signal)
