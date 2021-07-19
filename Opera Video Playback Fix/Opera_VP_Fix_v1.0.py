7142c57b152a911e88e04c8b7106b04d7120c5a6
Calixte Williams <calixte.williams@gmail.com>
2021-07-19 15:51:21 -0400

    -Fixed a spelling error in Destination -Trying to use os.path.expanduser() to resolve user pwd

--- Opera Video Playback Fix/Opera_VP_Fix_v1.0.py
+++ Opera Video Playback Fix/Opera_VP_Fix_v1.0.py
@@ -1,23 +1,27 @@
 #Import shutil module
+import os
 import shutil
 #Import path module from os
 from os import path
 #Path to working libffmpeg.so
-source_path_fix = "/home/calixte/Downloads/0.52.2-linux-x64/libffmpeg.so"
+source_path_fix = "/home/calixte/Downloads/Opera-Assets/libffmpeg.so"
+#file = os.path.expanduser("~/Downloads/Opera-Assets/libffmpeg.so")
 #Path to original libffmpeg.so in Opera
 source_path_original = "/usr/lib/x86_64-linux-gnu/opera/libffmpeg.so"
 #Checks if the source path is available
+
 if path.exists(source_path_fix):
+#if os.path.exists(file):
     #Path to backup and rename the original libffmpeg.so in Opera
-    destiniation_path_backup = "/usr/lib/x86_64-linux-gnu/opera/libffmpeg.so.bak"
+    destination_path_backup = "/usr/lib/x86_64-linux-gnu/opera/libffmpeg.so.bak"
     #Path to Opera Folder
-    destiniation_path = "/usr/lib/x86_64-linux-gnu/opera"
+    destination_path = "/usr/lib/x86_64-linux-gnu/opera"
     #Backup Original file to the same location
-    backup_original = shutil.move(source_path_original, destiniation_path_backup)
+    backup_original = shutil.move(source_path_original, destination_path_backup)
     #Copy fix to Opera
-    fix = shutil.copy(source_path_fix, destiniation_path)
+    fix = shutil.copy(source_path_fix, destination_path)
     #Print the message if operation successful
-    print(f"Opera Video Playback has been fixed. Moved fix to location: {destiniation_path}")
+    print(f"Opera Video Playback has been fixed. Moved fix to location: {destination_path}")
 else:
     #Print the message if the file does not exist
     print("Could not locate file. Operation Terminated.")
